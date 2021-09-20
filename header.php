5887951c20cc8811dbfdf7ffc062bd2500bddc1e
hamigakitanuki <kakizaki0213@gmail.com>
2021-09-21 01:56:18 +0900

    ナビリンク修正

--- header.php
+++ header.php
@@ -54,20 +54,21 @@
 			<?php else: ?>
 				<nav class="navbar navbar-expand-lg navbar-light bg-light">
 					<div class="container-fluid">
-						<a class="navbar-brand" href="#">お勉強タイム</a>
+						<a class="navbar-brand" href="<?php echo home_url( '/' ) ?>">お勉強タイム</a>
 						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon"></span>
 						</button>
 						<div class="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 								<li class="nav-item">
-									<a class="nav-link active" aria-current="page" href="#">トップ</a>
+									<a class="nav-link active" aria-current="page" href="<?php echo home_url( '/' ) ?>">トップ</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link" href="#">教材一覧</a>
+									<a class="nav-link" href="<?php echo home_url( '/' ) ?>textbook">教材一覧</a>
 								</li>
+
 								<li class="nav-item">
-									<a class="nav-link" href="#">問題一覧</a>
+									<a class="nav-link" href="<?php echo home_url( '/' ) ?>question">問題一覧</a>
 								</li>
 							</ul>
 							<!-- <form class="d-flex">
