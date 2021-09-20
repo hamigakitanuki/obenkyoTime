1346f7ecf9c5704cbaa563a6427a5cde8fb13b4f
hamigakitanuki <kakizaki0213@gmail.com>
2021-09-21 01:46:53 +0900

    ヘッダーナビ修正

--- header.php
+++ header.php
@@ -54,37 +54,26 @@
 			<?php else: ?>
 				<nav class="navbar navbar-expand-lg navbar-light bg-light">
 					<div class="container-fluid">
-						<a class="navbar-brand" href="#">Navbar</a>
+						<a class="navbar-brand" href="#">お勉強タイム</a>
 						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon"></span>
 						</button>
 						<div class="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 								<li class="nav-item">
-									<a class="nav-link active" aria-current="page" href="#">Home</a>
+									<a class="nav-link active" aria-current="page" href="#">トップ</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link" href="#">Link</a>
-								</li>
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-										Dropdown
-									</a>
-									<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-										<li><a class="dropdown-item" href="#">Action</a></li>
-										<li><a class="dropdown-item" href="#">Another action</a></li>
-										<li><hr class="dropdown-divider"></li>
-										<li><a class="dropdown-item" href="#">Something else here</a></li>
-									</ul>
+									<a class="nav-link" href="#">教材一覧</a>
 								</li>
 								<li class="nav-item">
-									<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+									<a class="nav-link" href="#">問題一覧</a>
 								</li>
 							</ul>
-							<form class="d-flex">
+							<!-- <form class="d-flex">
 								<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
 								<button class="btn btn-outline-success" type="submit">Search</button>
-							</form>
+							</form> -->
 						</div>
 					</div>
 				</nav>
